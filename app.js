b02878757270e08e64f858c39754789dd1cdedee
derek <derekdvalerio@gmail.com>
2026-08-21 18:16:01 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world");
+});
+
+app.listen(PORT, (error) => {
+  // This is important!
+  // Without this, any startup errors will silently fail
+  // instead of giving you a helpful error message.
+  if (error) {
+    throw error;
+  }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
