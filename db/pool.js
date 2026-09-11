948ee3f80aaaaec03b736388445bcdf307dc0750
derek <derekdvalerio@gmail.com>
2026-09-11 11:28:55 +0800

    chore: added ssl to connection pool

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
-const { Pool } = require("pg");
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
